750954d52b92dfee6d45f4357386829abeba058c
lihs-ie <lihs.dev@gmail.com>
2026-02-17 21:11:46 +0900

    feat(local): develop environment

---
+++ src/Forger.hs
@@ -0,0 +1,13 @@
+{- |
+Module      : Forger
+Copyright   : (c) 2026 lihs
+License     : MIT
+Maintainer  : lihs-ie
+
+A library for generating test data.
+This is the main entry point; it re-exports the public API.
+-}
+module Forger (
+    -- * Re-exports (to be added as modules are implemented)
+)
+where
---
+++ test/Forger/ForgerSpec.hs
@@ -0,0 +1,9 @@
+module Forger.ForgerSpec (spec) where
+
+import Test.Hspec
+
+spec :: Spec
+spec = do
+    describe "Forger" $ do
+        it "placeholder: library builds and tests run" $ do
+            True `shouldBe` True
